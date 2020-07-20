076c16767dd28b25d28a8e7b2e0e857ad363b6b7
victormarit <victormarit.95@gmail.com>
2020-07-20 13:24:10 +0200

    commentHashPassword

--- fonctions/fonctionsTk.py
+++ fonctions/fonctionsTk.py
@@ -9,6 +9,11 @@ def adminOrClient(fenetre, utilisateur):
         tk.homepageUser.HomepageUser(fenetre, utilisateur)
 
 def hashPassword(password):
+    """
+    Allows you to hash a password
+    Param : password(type) - string
+    Return : pw(type) - string ; length : 40 
+    """
     pw = hashlib.sha1(password.encode())
     pw = pw.hexdigest()
     return pw
